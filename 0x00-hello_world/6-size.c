baf5a3aac2da68bcb88987e302a7b92dba22752b
mamt4real <mamt4real@gmail.com>
2022-06-24 07:36:14 -0700

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	long long int x;
-	
+
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long));
