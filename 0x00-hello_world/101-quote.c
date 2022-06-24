73020bd092ca0faa034b12656c3e1ad385e16c81
mamt4real <mamt4real@gmail.com>
2022-06-24 07:54:35 -0700

    advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Description: This function prints a text
+ * Return: 0 to indicate success
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
+
