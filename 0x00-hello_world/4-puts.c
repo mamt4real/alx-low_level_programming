8bbd8759588e6f619c51e777a743c21f2be02878
mamt4real <mamt4real@gmail.com>
2022-06-24 05:05:42 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
+/*                                                * Description:  A column of asterisks on the left side,                                              * with beginning and ending almost-blank lines.                                                     */
 int main(void)
 {
-	/*
-	* Description:  A column of asterisks on the left side,
-	 * with beginning and ending almost-blank lines.
-	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
