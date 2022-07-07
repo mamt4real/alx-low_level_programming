81a8c27fac0db055dd3e1eda94d4c91a3880af90
mamt4real <mamt4real@gmail.com>
2022-07-07 08:02:13 -0700

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,15 +13,15 @@ int main(void)
 	for (i = 1; i < 101; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
-		else if (i == 100)
 			printf("Buzz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
