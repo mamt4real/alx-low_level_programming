10cb1d6aa527775525d18d3c7b8f342a22f4cc52
mamt4real <mamt4real@gmail.com>
2022-07-07 05:38:51 -0700

    Fixes

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,31 +12,11 @@ void more_numbers(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (c = 0; c <= 14; c++)
-			printNum(c);
+		{
+			if (c > 9)
+				_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+		}
 		_putchar('\n');
-	}
-}
-
-/**
- * printNum - prints an integer character wise
- * @n: the number to print
- */
-
-void printNum(int n)
-{
-	if (n < 0)
-	{
-		_putchar('-');
-		printNum(-n);
-		return;
-	}
-	if (n < 10)
-	{
-		_putchar(n + '0');
-	}
-	else
-	{
-		printNum(n / 10);
-		_putchar((n % 10) + '0');
 	}
 }
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,6 +18,8 @@ int main(void)
 			printf("Buzz ");
 		else if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
+		else if (i == 100)
+			printf("Buzz");
 		else
 			printf("%d ", i);
 	}
