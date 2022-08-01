758274cd67fce0cf99df7c15dd511b1cc8c50294
mamt4real <mamt4real@gmail.com>
2022-08-01 08:59:47 -0700

    fixes

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stddef.h>
 
 /**
  * init_dog - initializes a dog instance
@@ -9,6 +10,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,11 +14,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d;
 	char *t1, *t2;
 
-	t1 = malloc((strlen(name) + 1) * sizeof(char));
-	t2 = malloc((strlen(owner) + 1) * sizeof(char));
 	d = malloc(sizeof(*d));
-	if (d == NULL || t1 == NULL || t2 == NULL)
+	if (d == NULL)
 		return (NULL);
+	t1 = malloc((strlen(name) + 1) * sizeof(char));
+	if (t1 == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	t2 = malloc((strlen(owner) + 1) * sizeof(char));
+	if (t2 == NULL)
+	{
+		free(d);
+		free(t1);
+		return (NULL);
+	}
 	t1 = strcpy(t1, name);
 	t2 = strcpy(t2, owner);
 	d->age = age;
