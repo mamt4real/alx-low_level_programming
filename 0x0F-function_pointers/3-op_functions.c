088f3a52fd33c4bd2190c212521886b875c2a954
mamt4real <mamt4real@gmail.com>
2022-08-03 04:57:28 -0700

    fix division by zero

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -31,7 +31,10 @@ int op_sub(int a, int b)
 int op_mul(int a, int b)
 {
 	if (b == 0)
-		return (0);
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a * b);
 }
 
@@ -54,5 +57,10 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
