acc10ec09825905034b71740d87a1a4d48789cf1
mamt4real <mamt4real@gmail.com>
2022-08-03 04:42:43 -0700

    fix 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	if (b == NULL || a == NULL)
+	if (b == 0 && *(argv[2]) == '/')
 	{
 		printf("Error\n");
 		return (98);
