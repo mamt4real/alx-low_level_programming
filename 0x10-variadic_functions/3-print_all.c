1c07ed3cdd87309dd19f1420772f471ae73dbec2
mamt4real <mamt4real@gmail.com>
2022-08-04 09:06:44 -0700

    final fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,7 +13,7 @@
  */
 void print_all(const char * const format, ...)
 {
-	unsigned int i = 0;
+	unsigned int i = 0, hasprint = 0;
 	va_list args;
 
 	va_start(args, format);
@@ -23,21 +23,26 @@ void print_all(const char * const format, ...)
 		{
 			case 'i':
 				printf("%d", (int)va_arg(args, int));
+				hasprint = 1;
 				break;
 			case 'c':
 				printf("%c", (char)va_arg(args, int));
+				hasprint = 1;
 				break;
 			case 'f':
 				printf("%f", (float)va_arg(args, double));
+				hasprint = 1;
 				break;
 			case 's':
 				print_str((char *)va_arg(args, char *));
+				hasprint = 1;
 				break;
 			default:
+				hasprint = 0;
 				break;
 		}
 		i++;
-		if (format[i])
+		if (format[i] && hasprint)
 			printf(", ");
 	}
 	printf("\n");
