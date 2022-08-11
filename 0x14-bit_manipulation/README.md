8f9f7181072c2da321d30bf59aa4b384df07b587
mamt4real <mamt4real@gmail.com>
2022-08-11 08:11:16 -0700

    Task 0 and 1

---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,8 @@
+#Bitwise Operations
+
+& : Bitwise And Operator
+| : Bitwise Or Operator
+~ : Bitwise NOT Operator
+^ : Bitwise ExOr Operator
+>> : Bitwise Right Shift Operator
+<< : Bitwise Left Shift Operator
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef LIST_H
+#define LIST_H
+#include<stdio.h>
+#include<string.h>
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+void print_b_helper(unsigned long int, unsigned long int);
+#endif
