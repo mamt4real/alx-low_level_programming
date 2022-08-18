9b15a0f9ee9747b1455a47bd39ec955aa789f464
mamt4real <mamt4real@gmail.com>
2022-08-18 06:22:08 -0700

    Back to sq 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -21,11 +21,8 @@ void print_binary(unsigned long int n)
  */
 void print_b_helper(unsigned long int n, unsigned long int i)
 {
-	unsigned long int max = 1 << (MAX_COUNT - 1);
-
 	if (i > n)
 		return;
-	if (i < max)
-		print_b_helper(n, i << 1);
+	print_b_helper(n, i << 1);
 	_putchar(n & i ? '1' : '0');
 }
