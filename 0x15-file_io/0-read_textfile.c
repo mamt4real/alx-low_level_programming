ad714de0952a96ec489cde485eb35531886d039a
mamt4real <mamt4real@gmail.com>
2022-08-22 12:25:34 -0700

    fix 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,28 +1,41 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile - reads and prints content of a file
- * @filename: file name
- * @letters: number of letters to print
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
  *
- * Return: Number of chars printed or 0
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, retval, tmp;
-	char *buff;
+	 ssize_t o, r, w;
+	char *buffer;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
-	buff = malloc(sizeof(char) * letters);
-	if (!buff)
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer ==  NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	tmp = read(fd, buff, letters);
-	retval = write(1, buff, tmp);
-	free(buff);
-	if (fd == -1 || tmp == -1 || retval == -1 || retval != tmp)
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
 		return (0);
-	close(fd);
-	return (retval);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
+
 }
