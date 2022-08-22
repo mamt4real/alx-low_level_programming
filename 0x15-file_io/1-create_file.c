fab83843c2de3cff49a5aaabec28aa09dc61d4c6
mamt4real <mamt4real@gmail.com>
2022-08-22 09:38:58 -0700

    fix 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,13 +19,13 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 	{
 		close(fd);
-		return (-1);
+		return (1);
 	}
 	while (text_content[i])
 		i++;
 	retval = write(fd, text_content, i);
 	close(fd);
-	if (retval == -1)
+	if (retval == -1 || retval != i)
 		return (-1);
 	return (1);
 }
