5be04db89b4645dfd54a9321074a669621553de3
mamt4real <mamt4real@gmail.com>
2023-01-28 14:45:38 -0800

    Fix Task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,12 +10,13 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int i = 0;
+	size_t i = 0;
 
 	if (!array)
 		return (-1);
 	for (; i < size; i++)
 	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,6 +2,7 @@
 #define SEARCH_H
 
 #include <stdlib.h>
+#include <stdio.h>
 
 /* Prototypes */
 int linear_search(int *, size_t, int);
